f7dd6a825298e443b6eeae7213b377ebc561b93d
Joel Caleb Paytan Arias <joel.caleb.pa@gmail.com>
2016-09-26 13:44:53 -0500

    Ordenando el proyecto en capas

---
+++ src/main/java/com/caleb/businesslogic/ProductoBLogic.java
@@ -0,0 +1,27 @@
+package com.caleb.businesslogic;
+
+import java.util.List;
+
+import com.caleb.dao.ProductoDAOImpl;
+import com.caleb.entity.Categoria;
+import com.caleb.entity.Marca;
+import com.caleb.entity.Producto;
+
+public class ProductoBLogic {
+	
+	private ProductoDAOImpl dao = new ProductoDAOImpl();
+	
+	public void Registrar(Producto producto) {
+		dao.registrarProducto(producto);
+	}
+	public List<Producto> Listar() {
+		return dao.listarProductos();
+	}
+	public List<Producto> Listar(Marca marca) {
+		return dao.listarProductos(marca);
+	}
+	public List<Producto> Listar(Categoria categoria) {
+		return dao.listarProductos(categoria);
+	}
+
+}
